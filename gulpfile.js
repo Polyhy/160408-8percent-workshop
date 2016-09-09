ae34d8c23f426ee3d1cd1ce0ead6918315186588
polyhy <polyhy.gwon@gmail.com>
2016-09-09 18:28:02 +0900

    strip debug 와 uglify 추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var stripDebug = require('gulp-strip-debug');
 
 gulp.task('default', function() {
   return gulp.src('./src/*.js')
     .pipe(webpack(require('./webpack.config.js')))
     // webpack에서 처리해도 무방
+    .pipe(stripDebug())
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'))
 });
